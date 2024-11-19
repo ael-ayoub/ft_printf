f08d8dd88b3b692bb58cf9335b737ab0cc2a4747
Ayoub El aissaoui <ael-aiss@e1r13p9.1337.ma>
2024-11-19 11:37:40 +0100

    fff

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: ael-aiss <ael-aiss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 18:40:33 by ael-aiss          #+#    #+#             */
-/*   Updated: 2024/11/19 09:20:24 by ael-aiss         ###   ########.fr       */
+/*   Updated: 2024/11/19 11:31:41 by ael-aiss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	ft_putstr_fd(char *s, int fd)
 	int	i;
 	int	count;
 
+	if (!*s)
+		return (0);
 	count = 0;
 	i = 0;
 	while (s[i])
